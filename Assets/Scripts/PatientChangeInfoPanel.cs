976e9092e4a1fb49797b3eecd83cf72016c629f7
101010233005 <1010233005@qq.com>
2019-07-04 19:21:25 +0800

    lalala

---
+++ Assets/Scripts/PatientChangeInfoPanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatientChangeInfoPanel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ClickChangeInfoButton()
+    {
+        //修改个人信息
+        GameObject.Find("Canvas").GetComponent<MainMenuManager>().OpenPanelByName("PatientStartPanel");
+    }
+
+    public void ClickCancelButton()
+    {
+        GameObject.Find("Canvas").GetComponent<MainMenuManager>().OpenPanelByName("PatientStartPanel");
+    }
+
+    public void ClickUpdatePasswordButton()
+    {
+        GameObject.Find("Canvas").GetComponent<MainMenuManager>().OpenPanelByName("PatientUpdatePasswordPanel");
+    }
+}
